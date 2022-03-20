1d45e3f22ceafdeea53ef38863674fbaa40c64e6
Your Name <you@example.com>
2022-03-20 10:20:57 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Countries from "./components/Countries/Countries";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header></Header>
+      <Countries></Countries>
+      <GetCountries></GetCountries>
+    </div>
+  );
+}
+function GetCountries() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data.slice(0, 9)));
+  }, []);
+  console.log(countries);
+  return (
+    <div className="mm">
+      <h2>Available Country :{countries.length}</h2>
+      <div className="container">
+        {countries.map((country) => (
+          <GetCountry
+            Name={country.name.common}
+            capital={country.capital[0]}
+            flag={country.flags.png}
+          ></GetCountry>
+        ))}
+      </div>
+      <button className="btn3">Load More</button>
     </div>
   );
 }
 
+function GetCountry(props) {
+  return (
+    <div className="country">
+      <img src={props.flag} alt="" />
+      <h3 className="cName">Name: {props.Name}</h3>
+      <h4>Capital: {props.capital}</h4>
+      <div className="btns">
+        <button className="btn1">See More</button>
+        <button className="btn2">Remove</button>
+      </div>
+    </div>
+  );
+}
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="headerc">
+      <a href="./Home">Home</a>
+      <a href="./Shop">Shop</a>
+      <a href="./AboutUs">About Us</a>
+    </div>
+  );
+};
+
+export default Header;
