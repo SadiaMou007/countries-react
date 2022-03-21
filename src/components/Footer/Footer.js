8228e94b79a4775940e9bf82c0c9864e32bcf2e0
Your Name <you@example.com>
2022-03-21 02:50:59 -0700

    add footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer'><p><small> @Sadia Islam</small></p></div>
+  )
+}
+
+export default Footer
